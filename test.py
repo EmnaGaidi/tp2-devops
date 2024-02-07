204481e4f012f9f5f578dd61473678456f47f5b3
emna <emna.gaidii@gmail.com>
2024-02-07 23:40:18 +0100

    modification

--- test.py
+++ test.py
@@ -4,5 +4,7 @@ print("changement dans la branche hotfix/correction-bug ! :)")
 print("des modifications sur la branches bugfix")
 # -------
 print("des modifications sur la branches feature")
-#--------
+# --------
 print("des modifications sur la nouvelle branche crée par le collaborateur")
+# --------
+print("autre modification pour le pull request")
